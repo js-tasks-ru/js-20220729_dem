b3c45d041c7f39bd1189c07d5ab6f9dd65d357e8
demansh <demansh17@gmail.com>
2022-08-08 18:45:55 +0500

    02-sort-pick-omit omit is solved

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,7 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+  return Object.fromEntries(Object.entries(obj).filter(pair => {
+    return !fields.includes(pair[0]);
+  }));
 };
